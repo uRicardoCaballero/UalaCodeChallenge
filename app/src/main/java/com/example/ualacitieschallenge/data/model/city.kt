f705207ee0bab1881c0899f91e22c4c6169c8c91
Ricardo Caballero <68945049+uRicardoCaballero@users.noreply.github.com>
2025-04-01 20:21:59 -0600

    import and usage of room for data class

--- app/src/main/java/com/example/ualacitieschallenge/data/model/city.kt
+++ app/src/main/java/com/example/ualacitieschallenge/data/model/city.kt
@@ -1,16 +1,35 @@
 package com.example.ualacitieschallenge.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@Entity(tableName = "cities")
 data class City(
     val country: String,
     val name: String,
-    val id: Long,
-    val coord: Coordinates,
+    @PrimaryKey val id: Long,
+    @TypeConverters(CoordinatesConverter::class) val coord: Coordinates,
     var isFavorite: Boolean = false
 ) {
-    fun displayName() = "$name, $country"
+    fun displayName() = "$name, $country".lowercase()
 }
 
 data class Coordinates(
     val lon: Double,
     val lat: Double
 )
+
+class CoordinatesConverter {
+    @TypeConverter
+    fun fromCoordinates(coordinates: Coordinates): String {
+        return "${coordinates.lat},${coordinates.lon}"
+    }
+
+    @TypeConverter
+    fun toCoordinates(value: String): Coordinates {
+        val (lat, lon) = value.split(",")
+        return Coordinates(lon.toDouble(), lat.toDouble())
+    }
+}
